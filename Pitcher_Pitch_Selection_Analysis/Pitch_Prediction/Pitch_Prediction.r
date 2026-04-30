806ab1da085fc791af2c0a9733ad5309ab2a4ce6
jvillegas1021 <jvillegas1021@yahoo.com>
2026-04-30 10:14:47 -0400

    Add files via upload

--- Pitcher_Pitch_Selection_Analysis/Pitch_Prediction/Pitch_Prediction.r
+++ Pitcher_Pitch_Selection_Analysis/Pitch_Prediction/Pitch_Prediction.r
@@ -1,9 +1,9 @@
 
-predict_pitch_probabilities <- function(pitcher_df, static_model, balls = 0, strikes = 0, stance = 'R', runners_on = FALSE, runners_scoring_position = FALSE, tto = 1, prev_pitch = NA_character_, prev_result = NA_character_) {
+predict_pitch_probabilities <- function(pitcher_df, static_model, balls = 0, strikes = 0, stance = 'R', runners_on = FALSE, runners_scoring_position = FALSE, tto = 1, leverage = 'behind', prev_pitch = NA_character_, prev_result = NA_character_) {
     
     if (balls == 0 & strikes == 0) {
 
-        pitch_prediction <- predict_first_pitch(pitcher_df, stance, tto, runners_on, risp)
+        pitch_prediction <- predict_first_pitch(pitcher_df, stance, tto, runners_on, runners_scoring_position)
 
         # Convert to named numeric vector
         probs <- setNames(pitch_prediction$probability, pitch_prediction$pitch_type)
@@ -26,8 +26,9 @@ predict_pitch_probabilities <- function(pitcher_df, static_model, balls = 0, str
         pitch_base_runners_scoring_position_selection_multiplier_df <- pitch_base_runners_scoring_position_selection_multiplier_df(static_model, runners_scoring_position)
         
         pitch_tto_selection_multiplier_df <- pitch_tto_selection_multiplier_df(static_model, tto)
-
-
+        
+        pitch_count_leverage_selection_multiplier_df <- pitch_count_leverage_selection_multiplier_df(static_model, leverage)
+        
         pitch_prediction_df <- pitch_general_usage_selection_multiplier_df %>%
         left_join(
             previous_pitch_selection_multiplier_df,
@@ -53,8 +54,15 @@ predict_pitch_probabilities <- function(pitcher_df, static_model, balls = 0, str
             pitch_tto_selection_multiplier_df,
             by='pitch_type'
             ) %>%
+        left_join(
+            pitch_count_leverage_selection_multiplier_df,
+            by='pitch_type'
+            ) %>%
         mutate(
-            raw_score = (pitch_general_usage * prev_pitch_multiplier * prev_result_multiplier * pitch_count_multiplier * pitch_handedness_multiplier * base_runners_multiplier * base_runners_scoring_position_multiplier * pitch_tto_multiplier),
+            across(ends_with("_multiplier"), ~replace_na(.x, 1))
+            ) %>%
+        mutate(
+            raw_score = (pitch_general_usage * prev_pitch_multiplier * prev_result_multiplier * pitch_count_multiplier * pitch_handedness_multiplier * base_runners_multiplier * base_runners_scoring_position_multiplier * pitch_tto_multiplier * pitch_leverage_multiplier),
             probability = round(raw_score / sum(raw_score, na.rm=TRUE) * 100, 2)
             ) %>%
         select(
