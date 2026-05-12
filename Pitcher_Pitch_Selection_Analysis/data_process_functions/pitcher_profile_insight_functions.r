4d553d23e425eda3cd9a093b46d7cdb956a3116e
jvillegas1021 <jvillegas1021@yahoo.com>
2026-05-12 13:00:13 -0400

    Add files via upload

--- Pitcher_Pitch_Selection_Analysis/data_process_functions/pitcher_profile_insight_functions.r
+++ Pitcher_Pitch_Selection_Analysis/data_process_functions/pitcher_profile_insight_functions.r
@@ -1,11 +1,42 @@
 ###########################INSIGHTS############################################################
-lhb_insights <- function(lhb_pitcher_scounting_report_df)  {
-    return
+lhb_insights <- function(pitcher_scounting_report_df)  {
+    lhb_pitcher_scouting_report <- pitcher_scounting_report_df %>%
+    filter(stance == 'L')
+
+    lhb_count_leverage_insights <- get_count_leverage_insights(lhb_pitcher_scouting_report)
+    lhb_count_leverage_runners_on_insights <- get_count_leverage_runners_on_insights(lhb_pitcher_scouting_report)
+    lhb_count_leverage_runners_scoring_position_insights <- get_count_leverage_runners_scoring_position_insights(lhb_pitcher_scouting_report)
+    lhb_double_up_insights <- get_double_up_insights(lhb_pitcher_scouting_report)
+    lhb_follow_up_insights <- get_follow_up_insights(lhb_pitcher_scouting_report)
+    lhb_first_pitch_insights <- get_first_pitch_insights(lhb_pitcher_scouting_report)
+    
+    return(list(lhb_count_leverage_insights,
+                lhb_count_leverage_runners_on_insights,
+                lhb_count_leverage_runners_scoring_position_insights,
+                lhb_double_up_insights,
+                lhb_follow_up_insights,
+                lhb_first_pitch_insights))
     }
 
-rhb_insights <- function(rhb_pitcher_scounting_report_df)  {
-    return
+rhb_insights <- function(pitcher_scounting_report_df)  {
+    rhb_pitcher_scouting_report <- pitcher_scounting_report_df %>%
+    filter(stance == 'R')
+
+    rhb_count_leverage_insights <- get_count_leverage_insights(rhb_pitcher_scouting_report)
+    rhb_count_leverage_runners_on_insights <- get_count_leverage_runners_on_insights(rhb_pitcher_scouting_report)
+    rhb_count_leverage_runners_scoring_position_insights <- get_count_leverage_runners_scoring_position_insights(rhb_pitcher_scouting_report)
+    rhb_double_up_insights <- get_double_up_insights(rhb_pitcher_scouting_report)
+    rhb_follow_up_insights <- get_follow_up_insights(rhb_pitcher_scouting_report)
+    rhb_first_pitch_insights <- get_first_pitch_insights(rhb_pitcher_scouting_report)
+    
+    return(list(rhb_count_leverage_insights,
+                rhb_count_leverage_runners_on_insights,
+                rhb_count_leverage_runners_scoring_position_insights,
+                rhb_double_up_insights,
+                rhb_follow_up_insights,
+                rhb_first_pitch_insights))
     }
+    
 
 get_count_leverage_insights <- function(pitcher_scouting_report_df) {
     likely_pitches_count_leverage <- pitcher_scouting_report_df %>%
@@ -75,3 +106,5 @@ get_first_pitch_insights <- function(pitcher_scouting_report_df) {
     slice_max(most_likely_pitch, n=1)
     return(first_pitch_df)
     }
+
+
