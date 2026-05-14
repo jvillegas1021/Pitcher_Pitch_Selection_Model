98778b5357a82f957a03abe978bf932a772312f5
jvillegas1021 <jvillegas1021@yahoo.com>
2026-05-14 07:34:32 -0400

    Add files via upload

---
+++ Pitcher_Pitch_Selection_Analysis/data_extract_functions/data_extract_functions.r
@@ -0,0 +1,70 @@
+
+read_df_from_sql <- function(table_name) {
+    con <- dbConnect(
+        RPostgres::Postgres(),
+        host = Sys.getenv("DB_HOST"),
+        dbname = Sys.getenv("DB_NAME"),
+        user = Sys.getenv("DB_USER"),
+        password = Sys.getenv("DB_PASSWORD"),
+        port = as.integer(Sys.getenv("DB_PORT")),
+        sslmode = Sys.getenv("DB_SSLMODE")
+      )
+
+    df <- dbReadTable(con, table_name)
+
+    dbDisconnect(con)
+
+    return(df) 
+    }
+
+pull_pitcher_scouting_report_from_database <- function(pitcher_id) {
+
+    #scouting_report table name
+
+    table_name <- 'pitcher_scouting_reports'
+
+    query <- paste0('SELECT * FROM ', table_name, ' WHERE pitcher_id = ', pitcher_id)
+    
+    con <- dbConnect(
+        RPostgres::Postgres(),
+        host = Sys.getenv("DB_HOST"),
+        dbname = Sys.getenv("DB_NAME"),
+        user = Sys.getenv("DB_USER"),
+        password = Sys.getenv("DB_PASSWORD"),
+        port = as.integer(Sys.getenv("DB_PORT")),
+        sslmode = Sys.getenv("DB_SSLMODE")
+      )
+
+    df <- dbGetQuery(con, query)
+
+    dbDisconnect(con)
+
+    return(df)
+    }
+
+############# READ CSV FUNCTION####################
+pull_pitcher_statcast_data_from_database <- function(pitcher_id, pitch_type_list) {
+
+    table_name <- 'statcast_2023_2026'
+
+    query <- paste0('SELECT * FROM ', table_name, ' WHERE pitcher = ', pitcher_id)
+    
+    con <- dbConnect(
+        RPostgres::Postgres(),
+        host = Sys.getenv("DB_HOST"),
+        dbname = Sys.getenv("DB_NAME"),
+        user = Sys.getenv("DB_USER"),
+        password = Sys.getenv("DB_PASSWORD"),
+        port = as.integer(Sys.getenv("DB_PORT")),
+        sslmode = Sys.getenv("DB_SSLMODE")
+      )
+
+    df <- dbGetQuery(con, query)
+
+    dbDisconnect(con)
+    
+    filtered_statcast_df <- df %>%
+    filter(pitch_type %in% pitch_type_list)
+    
+    return(filtered_statcast_df)
+    }
