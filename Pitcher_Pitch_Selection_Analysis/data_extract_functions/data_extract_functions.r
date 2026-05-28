3c90428e375b0d6598c14f4cf5f43fa549ed6400
jvillegas1021 <jvillegas1021@yahoo.com>
2026-05-28 15:07:15 -0400

    Add files via upload

--- Pitcher_Pitch_Selection_Analysis/data_extract_functions/data_extract_functions.r
+++ Pitcher_Pitch_Selection_Analysis/data_extract_functions/data_extract_functions.r
@@ -42,6 +42,31 @@ pull_pitcher_scouting_report_from_database <- function(pitcher_id) {
     return(df)
     }
 
+pull_pitcher_info_from_database <- function(pitcher_id) {
+
+    #scouting_report table name
+
+    table_name <- 'active_pitcher_stats_current_year'
+
+    query <- paste0('SELECT * FROM ', table_name, ' WHERE "xMLBAMID" = ', pitcher_id)
+    
+    con <- dbConnect(
+        RPostgres::Postgres(),
+        host = Sys.getenv("DB_HOST"),
+        dbname = Sys.getenv("DB_NAME"),
+        user = Sys.getenv("DB_USER"),
+        password = Sys.getenv("DB_PASSWORD"),
+        port = as.integer(Sys.getenv("DB_PORT")),
+        sslmode = Sys.getenv("DB_SSLMODE")
+      )
+
+    df <- dbGetQuery(con, query)
+
+    dbDisconnect(con)
+
+    return(df)
+    }
+
 ############# READ CSV FUNCTION####################
 pull_pitcher_statcast_data_from_database <- function(pitcher_id, pitch_type_list) {
 
