42211feff38d5a408a0f8f16641810126bc01697
jvillegas1021 <jvillegas1021@yahoo.com>
2026-05-28 15:07:38 -0400

    Add files via upload

--- Pitcher_Pitch_Selection_Analysis/data_process_functions/pitcher_insight_functions.r
+++ Pitcher_Pitch_Selection_Analysis/data_process_functions/pitcher_insight_functions.r
@@ -20,6 +20,18 @@ create_lhb_and_rhb_insights <- function(pitcher_scounting_report_df)  {
     rhb_double_up_insights <- get_double_up_insights(rhb_pitcher_scouting_report)
     rhb_follow_up_insights <- get_follow_up_insights(rhb_pitcher_scouting_report)
     rhb_first_pitch_insights <- get_first_pitch_insights(rhb_pitcher_scouting_report)
+
+    rhb_early_count_insights <- get_early_count_probabilites(rhb_pitcher_scouting_report)
+    rhb_late_count_insights <- get_late_count_probabilites(rhb_pitcher_scouting_report)
+
+    lhb_early_count_insights <- get_early_count_probabilites(lhb_pitcher_scouting_report)
+    lhb_late_count_insights <- get_late_count_probabilites(lhb_pitcher_scouting_report)
+
+    rhb_after_hard_stuff_insights <- get_after_hard_stuff_probabilities(rhb_pitcher_scouting_report)
+    rhb_after_soft_stuff_insights <- get_after_soft_stuff_probabilities(rhb_pitcher_scouting_report)
+    lhb_after_hard_stuff_insights <- get_after_hard_stuff_probabilities(lhb_pitcher_scouting_report)
+    lhb_after_soft_stuff_insights <- get_after_soft_stuff_probabilities(lhb_pitcher_scouting_report)
+    
     
     return(list(lhb_count_leverage = lhb_count_leverage_insights,
                 lhb_runners_on = lhb_count_leverage_runners_on_insights,
@@ -32,7 +44,15 @@ create_lhb_and_rhb_insights <- function(pitcher_scounting_report_df)  {
                 rhb_scoring_position = rhb_count_leverage_runners_scoring_position_insights,
                 rhb_double_up = rhb_double_up_insights,
                 rhb_follow_up = rhb_follow_up_insights,
-                rhb_first_pitch = rhb_first_pitch_insights
+                rhb_first_pitch = rhb_first_pitch_insights,
+                rhb_early_count = rhb_early_count_insights,
+                rhb_late_count = rhb_late_count_insights,
+                lhb_early_count = lhb_early_count_insights,
+                lhb_late_count = lhb_late_count_insights,
+                rhb_after_hard_stuff = rhb_after_hard_stuff_insights,
+                rhb_after_soft_stuff = rhb_after_soft_stuff_insights,
+                lhb_after_hard_stuff = lhb_after_hard_stuff_insights,
+                lhb_after_soft_stuff = lhb_after_soft_stuff_insights
                ))
     }
 
@@ -107,4 +127,38 @@ get_first_pitch_insights <- function(pitcher_scouting_report_df) {
     return(first_pitch_df)
     }
 
+get_early_count_probabilites <- function(pitcher_scouting_report_df) {
+    early_count_df <- pitcher_scouting_report_df %>%
+    filter(balls + strikes <= 2) %>%
+    group_by(pitch_name) %>%
+    summarise(probability = round(mean(probability), 2))
+    return(early_count_df)
+    }
 
+get_late_count_probabilites <- function(pitcher_scouting_report_df) {
+    late_count_df <- pitcher_scouting_report_df %>%
+    filter(balls + strikes >= 3) %>%
+    group_by(pitch_name) %>%
+    summarise(probability = round(mean(probability), 2))
+    return(late_count_df)
+    }
+
+get_after_hard_stuff_probabilities <- function(pitcher_scouting_report_df) {
+    hard_pitch_types <- c('FF', 'FT', 'SI', 'FC')
+    after_hard_stuff_df <- pitcher_scouting_report_df %>%
+    filter(prev_pitch_type %in% hard_pitch_types) %>%
+    group_by(pitch_name) %>%
+    summarise(probability = round(mean(probability), 2)) %>%
+    arrange(desc(probability))
+    return(after_hard_stuff_df)
+    }
+
+get_after_soft_stuff_probabilities <- function(pitcher_scouting_report_df) {
+    hard_pitch_types <- c('FF', 'FT', 'SI', 'FC')
+    after_soft_stuff_df <- pitcher_scouting_report_df %>%
+    filter(!(prev_pitch_type %in% hard_pitch_types)) %>%
+    group_by(pitch_name) %>%
+    summarise(probability = round(mean(probability), 2)) %>%
+    arrange(desc(probability))
+    return(after_soft_stuff_df)
+    }
